1e45f38e3c4c945986e4c567b1edbc7b3921c131
Cyrille Berger <cberger@cberger.net>
2010-11-14 15:36:24 +0100

    reverse order now that they are right ordered

--- header.php
+++ header.php
@@ -61,9 +61,9 @@
         <div class="skip-link screen-reader-text"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentyten' ); ?>"><?php _e( 'Skip to content', 'twentyten' ); ?></a></div>
         <?php /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu.  The menu assiged to the primary position is the one used.  If none is assigned, the menu with the lowest ID is used.  */ ?>
         <div class="menu-header">
-          <img src="<?php bloginfo('stylesheet_directory'); ?>/images/buttonleft.png" />
-          <?php wp_nav_menu( array( 'container' => '', 'container_class' => '', 'theme_location' => 'primary' ) ); ?>
           <img src="<?php bloginfo('stylesheet_directory'); ?>/images/buttonright.png" />
+          <?php wp_nav_menu( array( 'container' => '', 'container_class' => '', 'theme_location' => 'primary' ) ); ?>
+          <img src="<?php bloginfo('stylesheet_directory'); ?>/images/buttonleft.png" />
         </div>
       </div><!-- #access -->
 			<div id="branding" role="banner">
