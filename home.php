33871a2ab0a5fba4d252be7691469d5202cdb815
Cyrille Berger <cberger@cberger.net>
2010-11-14 12:23:11 +0100

    add announcement/news

--- home.php
+++ home.php
@@ -1,6 +1,75 @@
 <?php
 get_header(); ?>
 
+<h2>Announcements</h2>
+<?php
+function showpost($category)
+{
+    rewind_posts();
+    while (have_posts()) :
+        the_post();
+        # if the post is in the category we want to exclude from the startpage but should show up in the category archives, we just skip to the next post.
+        if (!in_category($category)) continue; ?>
+
+        <div class="post" id="post-<?php the_ID(); ?>">
+            <h3><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+            <small><?php the_time('F jS, Y') ?></small>
+
+            <div class="entry">
+                <?php the_excerpt('Read the rest of this entry &raquo;'); ?>
+            </div>
+        </div>
+        <?php
+        break;
+    endwhile;
+}
+?>
+<?php
+    showpost('stable');
+//         showpost('unstable');
+//         showpost('freoffice');
+?>
+
+Read <a href="http://www.koffice.org/announcements/">more announcements...</a>
+
+<h2>News</h2>
+<?php
+rewind_posts();
+if (have_posts()) :
+    $counter = 0;
+    while (have_posts()) :
+        the_post();
+        # if the post is in the category we want to exclude from the startpage but should show up in the category archives, we just skip to the next post. -->       
+        if (!in_category('news') && !has_tag('frontpage') ) continue;
+        if (in_category("announcements")) continue;
+        ?>
+        <div class="post" id="post-<?php the_ID(); ?>">
+            <h3><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+            <small><?php the_time('F jS, Y') ?> <!-- by <?php the_author() ?> --></small>
+
+            <div class="entry">
+                <?php the_excerpt('Read the rest of this entry &raquo;'); ?>
+            </div>
+
+<!--                 <p class="postmetadata">Posted in <?php the_category(', ') ?> | <?php the_tags('Tags: ', ', ', '&nbsp;| '); ?> <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></p> -->
+        </div>
+        <?php
+        $counter += 1;
+        if ($counter > 2) break;
+    endwhile; ?>
+
+    <div class="navigation">
+            <div class="alignleft">Read <a href="news">more news</a></div>
+    </div>
+
+<?php else : ?>
+
+    <h2 class="center">Not Found</h2>
+    <p class="center">Sorry, but you are looking for something that isn't here.</p>
+    <?php include (TEMPLATEPATH . "/searchform.php"); ?>
+
+<?php endif; ?>
+
 
 <h2>Life stream</h2>
 <?php
