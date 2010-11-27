e4a8ffecc48f0fc127efec27b7b09d7a332517e8
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2010-11-27 12:02:03 +0100

    Add links to app pages in home banner

--- home_banner.php
+++ home_banner.php
@@ -1,70 +1,102 @@
           <div class="app_matrix">
             <div class="app big">
-              <img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/kword.png" width="128" height="128" alt="KWord icon" />
+              <a href="/kword" title="KWord word processor application">
+                <img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/kword.png" width="128" height="128" alt="KWord icon" />
+              </a>
               <div>
-                <span>KWord</span>
-                <br />
-                is an intuitive word processor application with desktop publishing features.
-                <br />
-                With it, you can create informative and attractive documents with ease.
+                <a href="/kword" title="KWord word processor application">
+                  <span>KWord</span>
+                  <br />
+                  is an intuitive word processor application with desktop publishing features.
+                  <br />
+                  With it, you can create informative and attractive documents with ease.
+                </a>
               </div>
             </div>
             <div class="app big center">
-              <img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/kspread.png" width="128" height="128" alt="KSpread icon" />
+              <a href="/kspread" title="KSpread spreadsheet application">
+                <img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/kspread.png" width="128" height="128" alt="KSpread icon" />
+              </a>
               <div>
-                <span>KSpread</span>
-                <br />
-                is a fully-featured spreadsheet application. Use it to quickly create spreadsheets with formulas and charts, to calculate and organize your data.
+                <a href="/kspread" title="KSpread spreadsheet application">
+                  <span>KSpread</span>
+                  <br />
+                  is a fully-featured spreadsheet application. Use it to quickly create spreadsheets with formulas and charts, to calculate and organize your data.
+                </a>
               </div>
             </div>
             <div class="app big">
-              <img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/kpresenter.png" width="128" height="128" alt="KPresenter icon" />
+              <a href="/kpresenter" title="KPresenter presentation application">
+                <img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/kpresenter.png" width="128" height="128" alt="KPresenter icon" />
+              </a>
               <div>
-                <span>KPresenter</span>
-                <br />
-                is a powerful and easy to use presentation application.
-                <br />
-                You can dazzle your audience with stunning slides containing images, videos, animation and more.
+                <a href="/kpresenter" title="KPresenter presentation application">
+                  <span>KPresenter</span>
+                  <br />
+                  is a powerful and easy to use presentation application.
+                  <br />
+                  You can dazzle your audience with stunning slides containing images, videos, animation and more.
+                </a>
               </div>
             </div>
             <div class="app small">
-              <img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/kexi.png" width="64" height="64" alt="Kexi icon" />
+              <a href="/kexi" title="Kexi data management application">
+                <img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/kexi.png" width="64" height="64" alt="Kexi icon" />
+              </a>
               <div>
-                <span>Kexi</span>
-                <br />
-                for integrated data management
+                <a href="/kexi" title="Kexi data management application">
+                  <span>Kexi</span>
+                  <br />
+                  for integrated data management
+                </a>
               </div>
             </div>
             <div class="app small center">
-              <img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/kivio.png" width="64" height="64" alt="Kivio icon" />
+              <a href="/kivio" title="Kivio diagram and flowchart application">
+                <img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/kivio.png" width="64" height="64" alt="Kivio icon" />
+              </a>
               <div>
-                <span>Kivio</span>
-                <br />
-                for diagramming and flowcharting
+                <a href="/kivio" title="Kivio diagram and flowchart application">
+                  <span>Kivio</span>
+                  <br />
+                  for diagramming and flowcharting
+                </a>
               </div>
             </div>
             <div class="app small">
-              <img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/karbon.png" width="64" height="64" alt="Karbon icon" />
+              <a href="/karbon" title="Karbon vector graphics application">
+                <img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/karbon.png" width="64" height="64" alt="Karbon icon" />
+              </a>
               <div>
-                <span>Karbon</span>
-                <br />
-                for drawing vector graphics
+                <a href="/karbon" title="Karbon vector graphics application">
+                  <span>Karbon</span>
+                  <br />
+                  for drawing vector graphics
+                </a>
               </div>
             </div>
             <div class="app small long">
-              <img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/kplato.png" width="64" height="64" alt="KPlato icon" />
+              <a href="/kplato" title="KPlato project planning application">
+                <img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/kplato.png" width="64" height="64" alt="KPlato icon" />
+              </a>
               <div>
-                <span>KPlato</span>
-                <br />
-                for project planning
+                <a href="/kplato" title="KPlato project planning application">
+                  <span>KPlato</span>
+                  <br />
+                  for project planning
+                </a>
               </div>
             </div>
             <div class="app small">
-              <img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/krita.png" width="64" height="64" alt="Krita icon" />
+              <a href="/krita" title="Krita image editing application">
+                <img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/krita.png" width="64" height="64" alt="Krita icon" />
+              </a>
               <div>
-                <span>Krita</span>
-                <br />
-                for painting and image editing
+                <a href="/krita" title="Krita image editing application">
+                  <span>Krita</span>
+                  <br />
+                  for painting and image editing
+                </a>
               </div>
             </div>
           </div>
